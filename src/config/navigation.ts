ad208d63cf90cb6275183cb60b5cd0c4623625b9
Jovert <jovertical@gmail.com>
2023-05-13 14:30:59 +0800

    feat: navigation icons update

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,14 +1,14 @@
 import {
-  BanknotesIcon,
-  BuildingStorefrontIcon,
+  ScaleIcon,
+  TicketIcon,
   Square3Stack3DIcon,
   Cog8ToothIcon,
 } from '@heroicons/react/24/outline';
 
 // prettier-ignore
 export const links = [
-  { name: 'Market', href: '/dashboard', icon: BuildingStorefrontIcon },
-  { name: 'My Bids', href: '/dashboard/my/bids', icon: BanknotesIcon },
+  { name: 'Market', href: '/dashboard', icon: ScaleIcon },
+  { name: 'My Bids', href: '/dashboard/my/bids', icon: TicketIcon },
   { name: 'My Listings', href: '/dashboard/my/listing', icon: Square3Stack3DIcon },
   { name: 'Settings', href: '/dashboard/settings', icon: Cog8ToothIcon },
 ];
