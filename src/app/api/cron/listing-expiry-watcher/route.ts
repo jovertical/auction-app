446626351b6283e964fb7283b3b400d19240dab9
Jovert <jovertical@gmail.com>
2023-05-15 01:03:12 +0800

    wip

--- src/app/api/cron/listing-expiry-watcher/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from 'next/server';
-
-import * as http from '@/utils/http/response';
-
-function handler(req: NextRequest) {
-  return http.notFound();
-}
-
-export { handler as GET, handler as POST };
